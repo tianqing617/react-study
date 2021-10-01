bf458784355be9e77071350b77c965806f7f240a
刘天青 <liutq@hollycrm.com>
2021-10-01 13:38:29 +0800

    complete weather

--- src/pages/demo/weather/index.jsx
+++ src/pages/demo/weather/index.jsx
@@ -2,13 +2,26 @@ import React, { Component } from 'react'
 import './index.scss'
 
 export default class Weather extends Component {
+  // 初始化状态
+  state = { isHot: false, wind: 'breeze' }
+
   render() {
+    const { isHot, wind } = this.state;
     return (
       <div className="weather">
         <span className="title">今天的天气：</span>
-        <span className="result">很热</span>
-        <button>切换</button>
+        <span>{ wind }，</span>
+        <span className="result">{ isHot ? 'hot' : 'cold' }</span>
+
+        <br/>
+        <button onClick={ this.changeWeather }>切 换</button>
       </div>
     )
+  }
+
+  // 自定义方法。箭头函数主要是为了 this 指向
+  changeWeather = () => {
+    const isHot = this.state.isHot;
+    this.setState({ isHot: !isHot });
   }
 }
