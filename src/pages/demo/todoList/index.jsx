f23e75d279c304a5ddf2f51c48c4d0d877334a46
刘天青 <liutq@hollycrm.com>
2021-10-07 23:39:14 +0800

    feat: delete todolist

--- src/pages/demo/todoList/index.jsx
+++ src/pages/demo/todoList/index.jsx
@@ -16,7 +16,7 @@ export default class TodoList extends Component {
     this.setState({ content: event.target.value });
   }
 
-  addItem = () => {
+  addTodoItem = () => {
     const { todoList } = this.state;
     const id = new Date().getTime();
     const item = {
@@ -30,6 +30,22 @@ export default class TodoList extends Component {
       ...todoList,
     ]});
   }
+  delTodoItem = (id) => {
+    const list = this.state.todoList.filter(item => {
+      return item.id !== id;
+    });
+    this.setState({ todoList: list });
+  }
+  addDustItem = (item) => {
+    const { dustList } = this.state;
+
+    this.setState({
+      dustList: [
+        item,
+        ...dustList,
+      ]
+    })
+  }
 
   render() {
     return (
@@ -38,11 +54,14 @@ export default class TodoList extends Component {
           {/* header */}
           <div className="todo-header">
             <Input className="input" onChange={this.saveContent} placeholder="Please input the task" />
-            <Button type="primary" onClick={this.addItem}>Add</Button>
+            <Button type="primary" onClick={this.addTodoItem}>Add</Button>
           </div>
 
           {/* body */}
-          <ListShow dataList={this.state.todoList} />
+          <ListShow dataList={this.state.todoList}
+            handleAddDustList={this.addDustItem.bind(this)}
+            handleDelTodoList={this.delTodoItem.bind(this)}
+          />
         </div>
         <ul className="dust">
           <ListShow dataList={this.state.dustList}/>
--- src/pages/demo/todoList/show.jsx
+++ src/pages/demo/todoList/show.jsx
@@ -2,6 +2,17 @@ import React, { Component } from 'react'
 import { Radio, Input } from 'antd';
 
 export default class ShowList extends Component {
+  saveDustList = (currentItem) => {
+    return () => {
+      // 向父组件传递
+      this.props.handleDelTodoList(currentItem.id);
+
+      this.props.handleAddDustList(
+        Object.assign(currentItem, { status: true })
+      );
+    }
+  }
+
   render() {
     const { dataList } = this.props;
 
@@ -11,7 +22,7 @@ export default class ShowList extends Component {
           dataList.map(item => {
             return (
               <li key={item.id}>
-                <Radio checked={item.status}></Radio>
+                <Radio checked={item.status} onChange={this.saveDustList(item)}></Radio>
                 <Input className="input" disabled value={item.content} />
               </li>
             )
