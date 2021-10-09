a8f31182bd6fb4287d0554aa0a25e69457b486e3
刘天青 <liutq@hollycrm.com>
2021-10-09 18:29:06 +0800

    feat: redux

--- src/pages/demo/counter/index.jsx
+++ src/pages/demo/counter/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { InputNumber, Button } from 'antd'
+import { createIncrementAction, createDecrementAction } from './redux/counterAction'
 import store from './redux/store'
 import './index.scss'
 
@@ -10,19 +11,27 @@ export default class Counter extends Component {
     this.setState({ num: value });
   }
 
+  increment = () => {
+		store.dispatch(createIncrementAction(this.state.num));
+    store.
+	}
+  decrement = () => {
+		store.dispatch(createDecrementAction(this.state.num));
+	}
+
   render() {
     return (
       <div className="counter">
         <div className="handler">
           <InputNumber className="input-number" min={1} max={10} defaultValue={store.getState()} onChange={this.saveNum} />
-          <Button shape="circle" >+</Button>
+          <Button shape="circle" onClick={this.increment}>+</Button>
           <Button shape="circle" >-</Button>
           <Button type="primary">奇数才加</Button>
           <Button type="primary">异步加</Button>
         </div>
         <div className="result">
           <span>=</span>
-          <span className="num">123</span>
+          <span className="num">{store.getState()}</span>
         </div>
       </div>
     )
