862f3fff7977b036a50f88abd16103b92e51b4d7
刘天青 <liutq@hollycrm.com>
2021-10-11 16:34:54 +0800

    feat: optimization

--- src/pages/demo/counterPerson/container.jsx
+++ src/pages/demo/counterPerson/container.jsx
@@ -2,16 +2,12 @@ import { connect } from 'react-redux'
 import Counter3 from './counter3'
 import { createIncrementAction, createDecrementAction, createIncrementAsyncAction } from './redux/action'
 
-function mapStateToProps(state) {
-  return { sum: state };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    increment: num => dispatch(createIncrementAction(num)),
-    decrement: num => dispatch(createDecrementAction(num)),
-    incrementAsync: (num, ms) => dispatch(createIncrementAsyncAction(num, ms))
+// 简写形式，详细写法见：counter2组件
+export default connect(
+  state => ({ sum: state }),
+  {
+    increment: createIncrementAction,
+    decrement: createDecrementAction,
+    incrementAsync: createIncrementAsyncAction
   }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter3);
+)(Counter3);
