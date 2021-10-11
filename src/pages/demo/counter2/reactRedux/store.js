062024fcf013220a7ed32bc6110ed98bb7c75a0a
刘天青 <liutq@hollycrm.com>
2021-10-11 10:41:32 +0800

    feat: support redux-extension

--- src/pages/demo/counter2/reactRedux/store.js
+++ src/pages/demo/counter2/reactRedux/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
 import counterReudcer from './counterReducer'
 import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-export default createStore(counterReudcer, applyMiddleware(thunk));
+export default createStore(counterReudcer, composeWithDevTools(applyMiddleware(thunk)));
