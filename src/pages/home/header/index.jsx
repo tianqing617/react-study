8a7895cc633469366b82af5de67f68c664316d2b
Tianqing Liu <lhard@qq.com>
2024-11-18 14:05:00 +0800

    feat: op

--- src/pages/home/header/index.jsx
+++ src/pages/home/header/index.jsx
@@ -12,6 +12,8 @@ export default class Header extends Component {
 
         <ul>
           <li>
+            <a href="https://zh-hans.react.dev/learn" rel="noreferrer" target="_blank">React官网</a>
+            {/* 老地址，不再使用 */}
             <a href="https://zh-hans.reactjs.org/docs/getting-started.html" rel="noreferrer" target="_blank">React官网</a>
           </li>
           <li>
